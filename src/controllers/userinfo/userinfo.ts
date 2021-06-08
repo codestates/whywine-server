8410254f6b6bb22e11ef56266ad9e069935c18d6
Youjin Lee <youjinlee1997@gmail.com>
2021-06-08 16:48:10 +0900

    HOTFIX: userinfo 와인에 태그 안담기는 문제 해결

--- src/controllers/userinfo/userinfo.ts
+++ src/controllers/userinfo/userinfo.ts
@@ -1,47 +1,56 @@
 import { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
-import { createQueryBuilder, getConnection,getRepository } from "typeorm";
+import { createQueryBuilder, getConnection, getRepository } from "typeorm";
 import { Tag } from "../../entity/tag";
 import { User } from "../../entity/user";
 import { Comment } from "../../entity/comment";
 import { Wine } from "../../entity/wine";
+import { resolve } from "path";
 
 dotenv.config();
 
 const userinfo = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    const userRepository = getRepository(User);
+    const wineRepository = getRepository(Wine);
+    const user = await userRepository.findOne({
+      where: { id: 7 }, //req.session!.passport!.user
+      relations: ["tags", "good", "bad", "wines"],
+    });
 
-    const userRepository = getRepository(User)
-    const user = await userRepository.find({
-        where: { id: req.session!.passport!.user },
-        relations: ["tags", "good", "bad", "wines"] });
     if (user) {
-      const userInfo = user[0]
+      let wineResult: Wine[] = [];
 
-
-      return res.status(200).send(
-        {
-          data: {
-            userInfo:{
-              id: userInfo.id,
-              email: userInfo.email,
-              nickname: userInfo.nickname,
-              image: userInfo.image,
-              likes: userInfo.likes,
-              bad: userInfo.bad,
-              good: userInfo.good,
-              tags: userInfo.tags,
-              wines: userInfo.wines,
-            }
-          },
-          message: "ok"
+      for (let wine of user.wines) {
+        let result = await wineRepository.findOne({
+          where: { id: wine.id },
+          relations: ["tags"],
+        });
+        if (result) {
+          wineResult.push(result);
         }
-      );
+      }
+      console.log(wineResult);
+      return res.status(200).send({
+        data: {
+          userInfo: {
+            id: user.id,
+            email: user.email,
+            nickname: user.nickname,
+            image: user.image,
+            likes: user.likes,
+            bad: user.bad,
+            good: user.good,
+            tags: user.tags,
+            wines: wineResult,
+          },
+        },
+        message: "ok",
+      });
     }
   } catch (error) {
     console.error(error.message);
     res.status(401).send({ data: null, message: "not authorized" });
-
   }
 };
 
