e57fda47cc57c70edb91a3777c8a44f9e832b63a
jeasong <cjs5241@gmail.com>
2021-06-08 11:35:23 +0900

    s3지역설정

--- src/routers/userinfo.ts
+++ src/routers/userinfo.ts
@@ -17,7 +17,7 @@ AWS.config.update({
   // aws 설정
   accessKeyId: process.env.S3_ACCESS_KEY_ID,
   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-  region: "us-east-2",
+  region: "ap-northeast-2",
 });
 const s3 = new AWS.S3();
 const upload = multer({
